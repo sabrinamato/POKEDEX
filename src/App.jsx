6b6c0177555e86e54ee764b038ac0bd010e7ee5a
Sabrina <128062252+sabrinamato00@users.noreply.github.com>
2023-03-16 19:56:08 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./Components/Main/Main";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
